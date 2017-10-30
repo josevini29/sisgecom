5c2ffa68c8bebeb05b6553831a10863e73bdeb1e
DESKTOP-F2GE9JV\Jose Vinicius <josevini29@hotmail.com>
2017-10-30 19:55:29 -0200

    Antes de renomear

--- src/br/integrado/jnpereira/nutrimix/controle/ProdutoController.java
+++ src/br/integrado/jnpereira/nutrimix/controle/ProdutoController.java
@@ -13,19 +13,19 @@ import br.integrado.jnpereira.nutrimix.tools.Data;
 import java.util.ArrayList;
 
 public class ProdutoController {
-    
+
     public final static String SAIDA = "S";
     public final static String ENTRADA = "E";
-    
+
     private final Dao dao = new Dao();
-    
+
     public void geraMovtoEstoque(MovtoEstoque movto) throws Exception {
         dao.autoCommit(false);
-        
+
         Produto produto = new Produto();
         produto.setCdProduto(movto.getCdProduto());
         dao.get(produto);
-        
+
         if (movto.getInCancelado() && movto.getCdEstqMovto() != null) {
             cancelaMovto(movto, produto);
         } else if (!movto.getInCancelado() && movto.getCdEstqMovto() == null) {
@@ -33,15 +33,15 @@ public class ProdutoController {
         } else {
             throw new Exception("Tipo de movimento de estoque não esperado.");
         }
-        
+
     }
-    
+
     private void geraMovto(MovtoEstoque movto, Produto produto) throws Exception {
-        
+
         movto.setQtEstoque(produto.getQtEstqAtual());
         movto.setVlCustoMedio(produto.getVlCustoMedio());
         movto.setInCancelado(false);
-        
+
         if (movto.getTpMovto().equals(ENTRADA)) {
             if (movto.getVlItem() == null) {
                 movto.setVlItem(0.00);
@@ -55,59 +55,67 @@ public class ProdutoController {
         } else {
             produto.setQtEstqAtual(produto.getQtEstqAtual() - movto.getQtMovto());
         }
-        
+
         dao.update(produto);
         dao.save(movto);
     }
-    
+
     private void cancelaMovto(MovtoEstoque movto, Produto produto) throws Exception {
         dao.get(movto);
         movto.setInCancelado(true);
         movto.setCdUserCancel(FrmMenuFXML.usuarioAtivo);
         movto.setDtCancelado(Data.getAgora());
-        
+
         if (movto.getTpMovto().equals(ENTRADA)) {
             produto.setQtEstqAtual(produto.getQtEstqAtual() - movto.getQtMovto());
-            //if (movto.getVlItem() != null) { //Calcula o custo médio
-            //double vlTotal = ((produto.getQtEstqAtual() * produto.getVlCustoMedio())
-            //       - (movto.getQtMovto() * movto.getVlItem()));
-            //produto.setVlCustoMedio(divisao(vlTotal, (produto.getQtEstqAtual() - movto.getQtMovto())));
-            //}
+            if (movto.getVlItem() != null) { //Calcula o custo médio
+                double vlTotal = ((produto.getQtEstqAtual() * produto.getVlCustoMedio())
+                        - (movto.getQtMovto() * movto.getVlItem()));
+                produto.setVlCustoMedio(divisao(vlTotal, (produto.getQtEstqAtual() - movto.getQtMovto())));
+            }
             dao.update(movto);
-            refazerMovtoEstoque(movto); //refaz o movimento de estoque                  
+            //refazerMovtoEstoque(movto); //refaz o movimento de estoque                  
         } else {
             dao.update(movto);
             produto.setQtEstqAtual(produto.getQtEstqAtual() + movto.getQtMovto());
         }
-        
+
         dao.update(produto);
     }
-    
+
     private void refazerMovtoEstoque(MovtoEstoque movto) throws Exception {
-        String where = "WHERE $cdProduto$ = " + movto.getCdProduto() + " AND $cdEstqMovto$ >= " + (movto.getCdEstqMovto() - 1) + " ORDER BY $cdEstqMovto$ ASC";
+        String where = "WHERE $cdProduto$ = " + movto.getCdProduto() + " AND $inCancelado$ = 'F' ORDER BY $cdEstqMovto$ ASC";
         ArrayList<Object> movtos = dao.getAllWhere(new MovtoEstoque(), where);
         double qtEstoqueInicial = 0.00;
         double vlCustoMedioInicial = 0.00;
         for (Object obj : movtos) {
             MovtoEstoque mov = (MovtoEstoque) obj;
             if (mov.getCdEstqMovto() < movto.getCdEstqMovto()) { //Ajuste anterior ao que irá ser estornado
-                qtEstoqueInicial = mov.getQtEstoque() + mov.getQtMovto();
-                if (movto.getVlItem() != null) {
-                    vlCustoMedioInicial = qtEstoqueInicial / ((mov.getVlItem() * mov.getQtMovto()) + (mov.getQtEstoque() * mov.getVlCustoMedio()));
+                if (mov.getTpMovto().equals(ENTRADA)) {
+                    qtEstoqueInicial = mov.getQtEstoque() + mov.getQtMovto();
+                    if (movto.getVlItem() != null) {
+                        vlCustoMedioInicial = divisao(qtEstoqueInicial, ((mov.getVlItem() * mov.getQtMovto()) + (mov.getQtEstoque() * mov.getVlCustoMedio())));
+                    }
+                } else {
+                    qtEstoqueInicial = mov.getQtEstoque() - mov.getQtMovto();
                 }
             }
-            if (mov.getCdEstqMovto() < movto.getCdEstqMovto() && !mov.getInCancelado()) {
+            if (mov.getCdEstqMovto() > movto.getCdEstqMovto()) {
                 mov.setQtEstoque(qtEstoqueInicial);
                 mov.setVlCustoMedio(vlCustoMedioInicial);
-                qtEstoqueInicial = mov.getQtEstoque() + mov.getQtMovto();
-                if (movto.getVlItem() != null) {                    
-                    vlCustoMedioInicial = qtEstoqueInicial / ((mov.getVlItem() * mov.getQtMovto()) + (mov.getQtEstoque() * mov.getVlCustoMedio()));
-                    dao.update(mov);
+                if (mov.getTpMovto().equals(ENTRADA)) {
+                    qtEstoqueInicial = mov.getQtEstoque() + mov.getQtMovto();
+                    if (movto.getVlItem() != null) {
+                        vlCustoMedioInicial = divisao(qtEstoqueInicial, ((mov.getVlItem() * mov.getQtMovto()) + (mov.getQtEstoque() * mov.getVlCustoMedio())));
+                    }
+                } else {
+                    qtEstoqueInicial = mov.getQtEstoque() - mov.getQtMovto();
                 }
             }
+            dao.update(mov);
         }
     }
-    
+
     private Double divisao(Double v1, Double v2) {
         if (v1 == null | v2 == null) {
             return 0.00;
@@ -117,7 +125,7 @@ public class ProdutoController {
         }
         return v1 / v2;
     }
-    
+
     public Double getUltimoPreco(Integer cdProduto) throws Exception {
         String where = "WHERE $cdProduto$ = " + cdProduto + " ORDER BY $dtPreco$ DESC";
         ArrayList<Object> precos = dao.getAllWhere(new PrecoProduto(), where);
@@ -142,47 +150,47 @@ public class ProdutoController {
         tipos.add(new TipoAjusteEstoque(9, "Doação", SAIDA));
         return tipos;
     }
-    
+
     public static TipoAjusteEstoque getTipoAjusteEstoque(int id) {
         return getAllTipoAjuste().get(id - 1);
     }
-    
+
     public static class TipoAjusteEstoque {
-        
+
         private Integer cdTpAjuste;
         private String dsTpAjuste;
         private String tpAjuste;
-        
+
         public TipoAjusteEstoque(Integer cdTpAjuste, String dsTpAjuste, String tpAjuste) {
             this.cdTpAjuste = cdTpAjuste;
             this.dsTpAjuste = dsTpAjuste;
             this.tpAjuste = tpAjuste;
         }
-        
+
         public Integer getCdTpAjuste() {
             return cdTpAjuste;
         }
-        
+
         public void setCdTpAjuste(Integer cdTpAjuste) {
             this.cdTpAjuste = cdTpAjuste;
         }
-        
+
         public String getDsTpAjuste() {
             return dsTpAjuste;
         }
-        
+
         public void setDsTpAjuste(String dsTpAjuste) {
             this.dsTpAjuste = dsTpAjuste;
         }
-        
+
         public String getTpAjuste() {
             return tpAjuste;
         }
-        
+
         public void setTpAjuste(String tpAjuste) {
             this.tpAjuste = tpAjuste;
         }
-        
+
         @Override
         public String toString() {
             return getDsTpAjuste();
@@ -199,42 +207,42 @@ public class ProdutoController {
         tipos.add(new TipoMovtoEstoque(3, "Ajuste"));
         return tipos;
     }
-    
+
     public static TipoMovtoEstoque getTipoMovtoEstoque(int id) {
         return getAllTipoMovtoEstoque().get(id);
     }
-    
+
     public static class TipoMovtoEstoque {
-        
+
         private Integer cdTpMovto;
         private String dsTpMovto;
-        
+
         public TipoMovtoEstoque(Integer cdTpMovto, String dsTpMovto) {
             this.cdTpMovto = cdTpMovto;
             this.dsTpMovto = dsTpMovto;
         }
-        
+
         @Override
         public String toString() {
             return getDsTpMovto();
         }
-        
+
         public Integer getCdTpMovto() {
             return cdTpMovto;
         }
-        
+
         public void setCdTpMovto(Integer cdTpMovto) {
             this.cdTpMovto = cdTpMovto;
         }
-        
+
         public String getDsTpMovto() {
             return dsTpMovto;
         }
-        
+
         public void setDsTpMovto(String dsTpMovto) {
             this.dsTpMovto = dsTpMovto;
         }
-        
+
     }
     //Tip de Movimento de Estoque
 
